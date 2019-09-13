ede616480f0f34ec5bea5850363ee0d24b314990
TB831 <druu831@gmail.com>
2019-09-13 12:53:50 -0700

    beginning stages of form built

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -3,6 +3,20 @@ import Nav from '../Components/Nav';
 import UserForm from './UserForm';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: '',
+      lastName: '',
+      address: {
+        line_1: '',
+        line_2: null,
+        city: '',
+        region: '',
+        postal: ''
+      }
+    }
+  }
   
   render() {
     return (
--- src/Containers/UserForm.js
+++ src/Containers/UserForm.js
@@ -3,8 +3,18 @@ import React, { Component } from 'react';
 class UserForm extends Component {
   render() { 
     return (
-      <div>
-        <h4>Insurance Form!</h4>
+      <div className="container">
+        <h4>Rating Information</h4>
+        <div className="row">
+          <div className="input-field col s6">
+            <input id="firstName" type="text" />
+            <label for="firstName" >First Name</label>
+          </div>
+          <div className="input-field col s6">
+            <input id="lastName" type="text" />
+            <label for="lastName">Last Name</label>
+          </div>
+        </div>
       </div>
     );
   }
