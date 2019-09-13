ecba9a68869089cbc9f3142ce1e56d4c9a6a5993
TB831 <druu831@gmail.com>
2019-09-13 13:02:22 -0700

    UserForm component now rendered with fields

--- src/Containers/UserForm.js
+++ src/Containers/UserForm.js
@@ -4,7 +4,7 @@ class UserForm extends Component {
   render() { 
     return (
       <div className="container">
-        <h4>Rating Information</h4>
+        <h5>Rating Information</h5>
         <div className="row">
           <div className="input-field col s6">
             <input id="firstName" type="text" />
@@ -13,6 +13,32 @@ class UserForm extends Component {
           <div className="input-field col s6">
             <input id="lastName" type="text" />
             <label for="lastName">Last Name</label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field col s12">
+            <input id="line_1" type="text" />
+            <label for="line_1">Address 1</label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field col s12">
+            <input placeholder="Optional" id="line_2" type="text" />
+            <label for="line_2">Address 2</label>
+          </div>
+        </div>
+        <div className="row">
+          <div className="input-field col s4">
+            <input id="city" type="text" />
+            <label for="city">City</label>
+          </div>
+          <div className="input-field col s4">
+            <input id="region" type="text" />
+            <label for="region">Region</label>
+          </div>
+          <div className="input-field col s4">
+            <input id="postal" type="text" />
+            <label for="postal">Postal Code</label>
           </div>
         </div>
       </div>
