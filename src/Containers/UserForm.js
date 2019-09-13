c31c25fb5cf18f4ee025fa4b640016385665a143
TB831 <druu831@gmail.com>
2019-09-13 11:17:44 -0700

    added UserForm boilerplate

---
+++ src/Containers/UserForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class UserForm extends Component {
+  render() { 
+    return (
+      <div>
+        <h4>Insurance Form!</h4>
+      </div>
+    );
+  }
+}
+ 
+export default UserForm;
